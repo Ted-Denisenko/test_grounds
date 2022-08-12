f995c73ba784ae131f5dca8e3387c396eb435a3c
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-08-12 11:03:20 +0700

    Убрал лишнюю проверку на переполнение контейнера Модифицирвоал проверки на переполнение для каждого типа контейнера - при значении, равном максимальному объему контейнера, будет выводить его объем, вместо рассчета объема содержимого Поменял функции нахождения арккосинуса на аналоги из boost, теперь можно работать с полными переменными(значение + ед. измерения), а не только с их значениями Площадь сектора круга теперь считается правильно Добавил исключение о несоотвествии вводимых данных(не обрабатывается) Объем содержимого горизонтального контейнера теперь расчитывается правильно

--- test_grounds/test_grounds.cpp
+++ test_grounds/test_grounds.cpp
@@ -8,6 +8,7 @@
 #include <boost/units/systems/si/prefixes.hpp>
 #include <boost/units/systems/angle/degrees.hpp>
 #include <cmath>
+#include <boost/units/cmath.hpp>
 
 using Volume = boost::units::quantity<boost::units::si::volume>;
 using Square = boost::units::quantity<boost::units::area_dimension>;
@@ -26,65 +27,60 @@ Volume ContentVolume(std::string tankType, double contentLevel_raw, double tankH
 	Volume contentVolume;
 
 
-	if (contentLevel_raw > tankHeight_raw)
+	
+	if (tankType == "v")
 	{
-		return (tankVolume);
+		if (contentLevel_raw >= tankHeight_raw)
+		{
+			return (tankVolume);
+		}
+		contentVolume = (3.1415 * tankRadius * tankRadius * contentLevel);
+		return contentVolume; //m^3
+	}
+	else if (tankType == "h")
+	{
+		if (contentLevel_raw >= tankDiameter_raw)
+		{
+			return (tankVolume);
+		}
+
+		quantity<plane_angle> sectorAngle(
+			2.0 * boost::units::acos((tankRadius - contentLevel) / tankRadius));
+
+		quantity<area> sectorSquare(
+			(sectorAngle.value() * tankRadius * tankRadius) / 2.0); //mm sqaured
+
+		quantity<length> triangleBase(
+			2.0*boost::units::sqrt(
+				(tankRadius * tankRadius) -
+				((tankRadius - contentLevel) *
+					(tankRadius - contentLevel)))); //
+
+		quantity<length> p(
+			(tankRadius + tankRadius + triangleBase) / 2.0);
+
+		quantity<area> triangleSquare(
+			boost::units::sqrt(
+				p *
+				(p - tankRadius) *
+				(p - tankRadius) *
+				(p - triangleBase)));
+
+		contentVolume = (sectorSquare - triangleSquare) * tankHeight;
+
+ 		return contentVolume; //m^3
 	}
 	else
 	{
-		if (tankType == "v")
-		{
-			if (contentLevel_raw > tankHeight_raw)
-			{
-				return (tankVolume);
-			}
-			contentVolume = (3.1415 * (tankRadius * tankRadius) * contentLevel);
-			return contentVolume; //m^3
-		}
-		else if (tankType == "h")
-		{
-			if (contentLevel_raw > (tankDiameter_raw/2.0))
-			{
-				return (tankVolume);
-			}
-
-			quantity<plane_angle> sectorAngle(((tankRadius.value() - tankHeight.value()) / tankRadius.value())
-					* radian);
-
-			quantity<area> sectorSquare(
-				(sectorAngle.value() * (tankRadius.value() * tankRadius.value()) / 2.0)
-				* milli * milli * square_meter); //mm sqaured
-
-			quantity<length> triangleBase(
-				sqrt(
-					(tankRadius.value() * tankRadius.value()) -
-					((tankRadius.value() - tankHeight.value()) *
-						(tankRadius.value() - tankHeight.value())))
-				* milli * meters);
-
-			quantity<length> p(
-				(tankRadius + tankRadius + triangleBase) / 2.0);
-
-			quantity<area> triangleSquare(
-				sqrt(
-					p.value() *
-					(p.value() - tankRadius.value()) *
-					(p.value() - tankRadius.value()) *
-					(p.value() - triangleBase.value()))
-				* milli * milli * square_meter );
-			contentVolume = (sectorSquare - triangleSquare) * tankHeight;
-
- 			return contentVolume; //m^3
-		}
+		throw std::exception("Invalid tank type. Only 'v' and 'h' accepted. ");
+		return contentVolume;
 	}
-	return contentVolume;
+	
+return contentVolume;
 }
 
 int main()
 {
-	std::string tankType = "h";
-	quantity<area> example(2.0 * milli * milli * square_meters);
-	std::cout << example << std::endl;
-	ContentVolume("h", 10.00, 10.00, 200.00);
+	ContentVolume("f", 2000, 1000, 2000);
 	return 0;
 }
